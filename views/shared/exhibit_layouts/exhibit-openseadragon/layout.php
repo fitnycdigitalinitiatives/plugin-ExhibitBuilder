29bf3e3b6a0097ab911e68c0f1385f5036599011
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-11-30 18:00:42 -0500

    nav

--- views/shared/exhibit_layouts/exhibit-openseadragon/layout.php
+++ views/shared/exhibit_layouts/exhibit-openseadragon/layout.php
@@ -9,20 +9,20 @@ $captionPosition = isset($options['captions-position'])
     ? html_escape($options['captions-position'])
     : 'center';
 ?>
-	<div class="row">
+  <nav>
+      <?php if ($prevLink = exhibit_builder_link_to_previous_page('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-circle previous'))): ?>
+        <?php echo $prevLink; ?>
+      <?php endif; ?>
+      <?php if ($nextLink = exhibit_builder_link_to_next_page('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-circle next'))): ?>
+        <?php echo $nextLink; ?>
+      <?php endif; ?>
+  </nav>
+  <div class="row">
 		<div class="col-sm-3">
 		<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
 		<div class="text-justify">
 			<?php echo $text; ?>
 		</div>
-		<nav>
-				<?php if ($prevLink = exhibit_builder_link_to_previous_page('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Prev', array('type' => 'button', 'class' => 'btn btn-default pull-left previous'))): ?>
-					<?php echo $prevLink; ?>
-				<?php endif; ?>
-				<?php if ($nextLink = exhibit_builder_link_to_next_page('Next <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default pull-right next'))): ?>
-					<?php echo $nextLink; ?>
-				<?php endif; ?>
-		</nav>
 		</div>
 		<div class="col-sm-9">
 		<?php $count = count($attachments); ?>
