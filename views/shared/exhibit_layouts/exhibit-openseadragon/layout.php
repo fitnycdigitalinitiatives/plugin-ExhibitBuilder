450cd0b4de89f9b89f7ab51d29322f6edc517b01
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-12-19 15:15:25 -0500

    modal

--- views/shared/exhibit_layouts/exhibit-openseadragon/layout.php
+++ views/shared/exhibit_layouts/exhibit-openseadragon/layout.php
@@ -45,7 +45,26 @@ $captionPosition = isset($options['captions-position'])
 							<?php if ($caption): ?>
 							<div class="caption">
 								<small><?php echo $caption; ?></small>
-                <span class="glyphicon glyphicon-info-sign" aria-hidden="true" data-toggle="popover" data-placement="top" title="<?php echo metadata($item, array('Dublin Core', 'Title')); ?>" data-content='<?php echo link_to_item('View full record', $props = array(), $action = 'show', $item); ?>'></span>
+                <!-- Modal button -->
+                <span class="glyphicon glyphicon-info-sign" aria-hidden="true" data-toggle="modal" data-target="#record-modal<?php echo $id; ?>"></span>
+                <!-- Record Modal -->
+                <div class="modal fade" id="record-modal<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel"><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h4>
+                      </div>
+                      <div class="modal-body">
+                        <?php echo all_element_texts($item); ?>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <?php echo link_to_item('View full record', $props = array('class' => 'btn btn-primary', 'role' => 'button'), $action = 'show', $item); ?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 							</div>
 							<?php endif; ?>
 						</div>
