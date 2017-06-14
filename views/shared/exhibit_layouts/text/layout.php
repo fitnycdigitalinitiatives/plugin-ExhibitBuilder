286b400afb9db9470aa66a501e13b3dec6d72abe
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-06-14 15:14:05 -0400

    updated nav for text layout

--- views/shared/exhibit_layouts/text/layout.php
+++ views/shared/exhibit_layouts/text/layout.php
@@ -1,16 +1,20 @@
-<div class="row">	
+<div class="row">
 		<div class="col-sm-9">
 			<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
 			<div class="text-justify">
 				<?php echo $text; ?>
 			</div>
-			<nav>
-				<?php if ($prevLink = exhibit_builder_link_to_previous_page('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Prev', array('type' => 'button', 'class' => 'btn btn-default pull-left previous'))): ?>
-					<?php echo $prevLink; ?>
-				<?php endif; ?>
-				<?php if ($nextLink = exhibit_builder_link_to_next_page('Next <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default pull-right next'))): ?>
-					<?php echo $nextLink; ?>
-				<?php endif; ?>
-			</nav>
 		</div>
+</div>
+<div class="row exhibit-nav">
+	<div class="col-xs-12">
+		<?php if ($prevLink = exhibit_builder_link_to_previous_page('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round previous'))): ?>
+			<?php echo $prevLink; ?>
+		<?php else: ?>
+			<?php echo exhibit_builder_link_to_exhibit(null, '<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round previous')); ?>
+		<?php endif; ?>
+		<?php if ($nextLink = exhibit_builder_link_to_next_page('<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round next'))): ?>
+			<?php echo $nextLink; ?>
+		<?php endif; ?>
+	</div>
 </div>
