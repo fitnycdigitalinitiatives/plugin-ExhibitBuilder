19f0e8101a6b6f85a8263fa53d20c85b72c52bd7
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-12 10:16:55 -0400

    made text layout smaller

--- views/shared/exhibit_layouts/text/layout.php
+++ views/shared/exhibit_layouts/text/layout.php
@@ -1,5 +1,5 @@
 <div class="row">
-		<div class="col-sm-9">
+		<div class="col-sm-8">
 			<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
 			<div class="text-justify">
 				<?php echo $text; ?>
