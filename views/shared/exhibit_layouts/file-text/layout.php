c2c6e6622d2034700af79c8e435edc7592823812
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-27 10:56:03 -0400

    fixing exhibit layout

--- views/shared/exhibit_layouts/file-text/layout.php
+++ views/shared/exhibit_layouts/file-text/layout.php
@@ -13,6 +13,33 @@ $captionPosition = isset($options['captions-position'])
     <?php foreach ($attachments as $attachment): ?>
       <?php $item = $attachment->getItem(); ?>
         <?php echo mdid_image_tag($item, 'img-responsive'); ?>
+        <?php $caption = $attachment['caption']; ?>
+        <div class="caption">
+          <small><?php echo $caption; ?></small>
+          <!-- Modal button -->
+          <span class="glyphicon glyphicon-info-sign" aria-hidden="true" data-toggle="modal" data-target="#record-modal<?php echo $id; ?>"></span>
+          <!-- Record Modal -->
+          <div class="modal fade" id="record-modal<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 class="modal-title" id="myModalLabel">Item Information</h4>
+                </div>
+                <div class="modal-body">
+                  <?php echo all_element_texts($item); ?>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  <?php echo link_to_item('View full record', $props = array('class' => 'btn btn-primary', 'role' => 'button'), $action = 'show', $item); ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     <?php endforeach; ?>
 </div>
-<?php echo $text; ?>
+<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
+<div class="text-justify">
+  <?php echo $text; ?>
+</div>
